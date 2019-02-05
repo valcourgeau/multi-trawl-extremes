2f56ad4a2a03debc3a096640858f6275dd2e53f1
Valentin Courgeau <valentin.courgeau@free.fr>
2019-02-05 11:29:40 +0000

    Cured function to perfrom vine fitting and matrices creation

---
+++ r_files/multi_ev.R
@@ -0,0 +1,259 @@
+# packages
+require(hypergeo)
+require(ev.trawl)
+require(lubridate)
+require(magrittr)
+require(rlist)
+require(stats)
+require(evir)
+source("prep_univariate_latent_trawl_fit.R")
+
+#' @examples 
+#' ok <- ignoreColStartingWith(data = energy_weather_merged, tags = c("humidity",
+#'                                                                  "wind_direction"))
+ignoreColStartingWith <- function(data, tags, return.filters=F){
+  # return.filters 
+  filtered_by_tag <- sapply(tags, function(x){grepl(x, colnames(data))})
+  filtered_by_tag <- apply(filtered_by_tag, 
+                           MARGIN = 1, 
+                           FUN = function(x){Reduce('|',x)}) # merging the filters
+  if(return.filters){
+    return(!filtered_by_tag)
+  }
+  return(data[,!filtered_by_tag]) 
+}
+
+
+#' @examples cols_to_ignore <- c("datetime", "index.x", "index.y")
+#' types_to_ignore <- c("factor")
+#' tags_to_ignore <- c("humidity",
+#'                     "wind_direction")
+#' 
+#' core_energy_data <- getCoreData(data = energy_weather_merged, 
+#'                                 ignore_tags = tags_to_ignore,
+#'                                 ignore_cols = cols_to_ignore,
+#'                                 ignore_data_type = types_to_ignore)
+getCoreData <- function(data, ignore_tags=c(), ignore_cols=c(), ignore_data_type=c()){
+  return(data[, ignoreColStartingWith(data, ignore_tags, return.filters = T) &
+                (!colnames(data) %in% ignore_cols) & 
+                !(sapply(data, class) %in% ignore_data_type)])
+}
+
+
+
+# dates is a vector of datetime
+# data is a vector of data 
+require(stats)
+deterministicCleaning <- function(dates, data, p.adjust.method="bonferroni"){
+  dates <- strptime(energy_weather_merged$datetime, "%Y-%m-%d %H:%M:%S", tz = "GMT")
+  fitting_matrix <- cbind(cos(2*pi*1:length(data)/24),
+                          sin(2*pi*1:length(data)/24),
+                          cos(2*pi*1:length(data)/(24*365)),
+                          sin(2*pi*1:length(data)/(24*365)),
+                          #as.numeric(isWeekend(dates)==T),
+                          vapply(1:3, 
+                                 FUN = function(i){quarter(dates) == i}, 
+                                 FUN.VALUE = quarter(dates)),
+                          vapply(1:12, 
+                                 FUN = function(i){month(dates) == i}, 
+                                 FUN.VALUE = month(dates)),
+                          vapply(1:6, 
+                                 FUN = function(i){wday(dates) == i}, 
+                                 FUN.VALUE = wday(dates)),
+                          vapply(1:23, 
+                                 FUN = function(i){hour(dates) == i}, 
+                                 FUN.VALUE = hour(dates)))
+  fit <- lm(data ~ fitting_matrix)
+  
+  # adjusting the method to take in account multiple p.values testing
+  p.v.adjusted <- summary(fit)$coefficients[,4]
+  p.v.adjusted <- p.adjust(p.v.adjusted, method = p.adjust.method)
+  # TODO test that!
+  # print(p.v.adjusted)
+  
+  fitting_indices <- which(p.v.adjusted < 0.05)
+  print(length(fitting_indices))
+  if(1 %in% fitting_indices){
+    fitting_indices <- fitting_indices[-1]
+  }
+  fitting_matrix <- fitting_matrix[,fitting_indices-1]
+  return(lm(data ~ fitting_matrix)$residuals)
+}
+
+#' @examples 
+#' test <- datasetCleaning(data = core_energy_data,
+#'                         dates = energy_weather_merged$datetime)
+datasetCleaning <- function(data, dates){
+  result <- apply(data, MARGIN = 2, 
+                  FUN = function(x){
+                    deterministicCleaning(data = x, dates = dates)
+                  }
+  )
+  result <- as.data.frame(result)
+  colnames(result) <- colnames(data)
+  return(result)
+}
+
+
+findUnivariateParams <- function(data, clusters_size){
+  n_vars <- length(data[1,])
+  val_params <- matrix(0, nrow = n_vars, ncol = 4)
+  for(i_agent in 1:n_vars){
+    print(i_agent)
+    tryCatch(
+      val_params[i_agent,] <- generate_parameters(data[,i_agent],
+                                                  cluster.size = clusters_size[i_agent])
+      , error = function(e) {
+        val_params[i_agent,] <- 0
+      })
+  }
+  return(val_params)
+}
+
+
+getThresholds <- function(data, p.exceed){
+  if(p.exceed < 0 | p.exceed > 1)
+    stop('p.exceed should be between 0 and 1.')
+  
+  # deals with data as a vector
+  if(is.vector(data)){
+    return(quantile(data, p.exceed[1]))
+  }
+  
+  if(length(p.exceed) == 1){
+    return(sapply(data, function(x){quantile(x, p.exceed)}))
+  }else{
+    if(length(data[1,]) != p.exceed){
+      return(sapply(data, function(x){quantile(x, p.exceed)}))
+    }else{
+      stop('p.exceed should either be a scalar or as wide as data.')
+    }
+  }
+}
+
+#' @examples
+#' threshold_data <- makeExceedances(test, thresholds = getThresholds(test, 0.8))
+makeExceedances <- function(data, thresholds, normalize=TRUE){
+  if(length(data[1,]) != length(thresholds)){
+    stop('thresholds and data have non-comforting sizes. 
+         Tip: Use getThresholds to get the right size.')
+  }
+  
+  if(!is.vector(data)){
+    n <- length(data[,1])
+    rep_thres <- t(matrix(rep(thresholds, n), ncol=n))
+    epd <- (data - rep_thres) * (data > rep_thres)
+  }else{
+    epd <- (data - thresholds) * (data > thresholds)
+    
+  }
+  if(normalize){
+    epd <- apply(X = as.matrix(epd), MARGIN = 2, 
+                 FUN = function(x){return(x/sd(x))})
+    
+  }  
+  epd <- data.frame(epd)
+  colnames(epd) <- colnames(data)
+  return(epd)
+}
+
+
+
+plgpd_unif_at_zero <- function(x, p.zero, alpha, beta, kappa){
+  if(p.zero < 0 | p.zero > 1) stop("p.zero should be between 0 and 1.")
+  if(x == 0)
+    return(runif(n = 1, min = 0, max = p.zero))
+  else{
+    return(p.zero + (1-p.zero)*(1-max(0, (1+sign(alpha)*x/(beta+kappa))^{-alpha})))
+  }
+}
+
+plgpd_unif_at_zero.row <- function(xs, p.zeroes, params.mat){
+  # params.mat contains alpha beta rho kappa
+  res <- rep(0, length(xs))
+  for(i in 1:length(xs)){
+    res[i] <- plgpd_unif_at_zero(x = xs[i],
+                                 p.zero = p.zeroes[i],
+                                 alpha = params.mat[i,1],
+                                 beta = params.mat[i,2],
+                                 kappa = params.mat[i,4])
+  }
+  
+  return(res)
+}
+
+computePZero <- function(params){
+  # Computes probability of having zero given univariate extreme value model
+  return(1-(1+params[,4]/params[,2])^{-abs(params[,1])})
+}
+
+makeConditionalMatrices <- function(data, exceedeances, q.s, 
+                                    horizon, save=T, params, n_samples = length(data[,1]),
+                                    name="conditional-matrices"){
+  
+  p.zeroes <- computePZero(params)
+  # epd_cdf <- apply(X = exceedeances, MARGIN = 1, 
+  #                  FUN = function(x){
+  #                    return(plgpd.row(xs = x, p.zeroes = p.zeroes, params.mat = params))
+  #                    }
+  #                  )
+  # epd_cdf <- t(epd_cdf)
+  
+  exceedeances_cdf_ecdf <- exceedeances
+  for(i in 1:length(exceedeances[1,])){
+    # This creates ordered uniform samples in the same order 
+    # as in data.
+    exceedeances_cdf_ecdf[which(exceedeances[,i]==0), i] <- 
+      ecdf(data[which(exceedeances[,i]==0), i])(data[which(exceedeances[,i]==0), i]) * p.zeroes[i]
+  }
+  
+  list_of_list_horizons <- list()
+  n_vars <- length(data[1,])
+  for(h in horizon){
+    list_of_matrices_conditional <- list()
+    # creates a square matrix with all the vars in
+    quantile.update.values <- matrix(0, 
+                                     nrow = length(exceedeances[1,]), 
+                                     ncol = length(exceedeances[1,]))
+    colnames(quantile.update.values) <- colnames(exceedeances)
+    rownames(quantile.update.values) <- colnames(exceedeances)
+    
+    for(i in 1:n_vars){
+      # creates a temporary matrix with cols equal to number of nvars + 1
+      # and rows such that the i-th component is an extreme
+      mat_temp <- matrix(0,
+                         nrow = length(which(exceedeances[1:(s.sample-h), i] > 0)),
+                         ncol = n_vars+1)
+      # filtering the i-th eCDF column with extremes 
+      temp <- exceedeances_cdf_ecdf[which(exceedeances[1:(s.sample-h), i] > 0), i]
+      # addting those values in the nvars + 1 column
+      mat_temp[,n_vars+1] <- ecdf(temp)(temp)
+      
+      for(j in 1:n_vars){
+        # filtering data of j-th vars when i-th is extreme h timesteps before
+        data_j <- exceedeances_cdf_ecdf[which(exceedeances[1:(s.sample-h), i] > 0)+h, j]
+        # computing the probability that j-th was an extreme as well 
+        # h timesteps after i-th was an extreme
+        quantile.update.values[i, j] <- mean(data_j <= q.s[j])
+        data_j <- ecdf(data_j)(data_j)
+        # saving the unif values of j-th var used here.
+        mat_temp[,j] <- data_j 
+      }
+      
+      colnames(mat_temp) <- c(colnames(exceedeances), colnames(exceedeances)[i])
+      list_of_matrices_conditional[[i]] <- mat_temp
+    }
+    
+    list_of_list_horizons[[h]] <- list(unif.values=list_of_matrices_conditional,
+                                       quantiles.values=quantile.update.values)
+  }
+  
+  if(save){
+    file_name <- paste(name, ".RData", sep="")
+    cat(paste("Backing up the conditional matrices as", file_name, "..."))
+    list.save(list_of_list_horizons,
+              file=file_name)
+    cat("done\n")
+  }
+  return(list_of_list_horizons)
+}
--- r_files/weather_energy_multi_ev.R
+++ r_files/weather_energy_multi_ev.R
@@ -5,33 +5,18 @@ require(lubridate)
 require(magrittr)
 require(rlist)
 
+source("multi_ev.R")
+
 # loading data
 merged_dataset_folder <- "C:/Users/Valentin/Documents/GitHub/multi-trawl-extremes/data/merged-datasets/"
 setwd(merged_dataset_folder)
 load("energy-weather-merge.Rda")
 
-
-ignoreColStartingWith <- function(data, tags, return.filters=F){
-  # return.filters 
-  filtered_by_tag <- sapply(tags, function(x){grepl(x, colnames(data))})
-  filtered_by_tag <- apply(filtered_by_tag, 
-                           MARGIN = 1, 
-                           FUN = function(x){Reduce('|',x)}) # merging the filters
-  if(return.filters){
-    return(!filtered_by_tag)
-  }
-  return(data[,!filtered_by_tag]) 
-}
 ok <- ignoreColStartingWith(data = energy_weather_merged, tags = c("humidity",
                                                                    "wind_direction"))
 dim(ok)
 colnames(energy_weather_merged)
 
-getCoreData <- function(data, ignore_tags=c(), ignore_cols=c(), ignore_data_type=c()){
-  return(data[, ignoreColStartingWith(data, ignore_tags, return.filters = T) &
-                (!colnames(data) %in% ignore_cols) & 
-                !(sapply(data, class) %in% ignore_data_type)])
-}
 cols_to_ignore <- c("datetime", "index.x", "index.y")
 types_to_ignore <- c("factor")
 tags_to_ignore <- c("humidity",
@@ -46,7 +31,7 @@ dim(test)
 
 # dates is a vector of datetime
 # data is a vector of data 
-require(stats)
+
 deterministicCleaning <- function(dates, data, p.adjust.method="bonferroni"){
   dates <- strptime(energy_weather_merged$datetime, "%Y-%m-%d %H:%M:%S", tz = "GMT")
   fitting_matrix <- cbind(cos(2*pi*1:length(data)/24),
@@ -102,80 +87,11 @@ plot(test$pressure.Miami, type='l')
 setwd("C:/Users/Valentin/Documents/GitHub/multi-trawl-extremes/r_files/")
 source("prep_univariate_latent_trawl_fit.R")
 library("evir")
-s.clusters <- c(10, 15, 11, 11, 13, 13)
 
-findUnivariateParams <- function(data, clusters_size){
-  n_vars <- length(data[1,])
-  val_params <- matrix(0, nrow = n_vars, ncol = 4)
-  for(i_agent in 1:n_vars){
-    print(i_agent)
-    tryCatch(
-      val_params[i_agent,] <- generate_parameters(data[,i_agent],
-                                                cluster.size = clusters_size[i_agent])
-    , error = function(e) {
-      val_params[i_agent,] <- 0
-    })
-  }
-  return(val_params)
-}
-
-dim(test)
-dim(energy_weather_merged)
-
-getThresholds <- function(data, p.exceed){
-  if(p.exceed < 0 | p.exceed > 1)
-    stop('p.exceed should be between 0 and 1.')
-  
-  # deals with data as a vector
-  if(is.vector(data)){
-    return(quantile(data, p.exceed[1]))
-  }
-  
-  if(length(p.exceed) == 1){
-    return(sapply(data, function(x){quantile(x, p.exceed)}))
-  }else{
-    if(length(data[1,]) != p.exceed){
-      return(sapply(data, function(x){quantile(x, p.exceed)}))
-    }else{
-      stop('p.exceed should either be a scalar or as wide as data.')
-    }
-  }
-}
-
-makeExceedances <- function(data, thresholds, normalize=TRUE){
-  if(length(data[1,]) != length(thresholds)){
-    stop('thresholds and data have non-comforting sizes. 
-         Tip: Use getThresholds to get the right size.')
-  }
-  
-  if(!is.vector(data)){
-    n <- length(data[,1])
-    rep_thres <- t(matrix(rep(thresholds, n), ncol=n))
-    epd <- (data - rep_thres) * (data > rep_thres)
-  }else{
-    epd <- (data - thresholds) * (data > thresholds)
-    
-  }
-  if(normalize){
-    epd <- apply(X = as.matrix(epd), MARGIN = 2, 
-                 FUN = function(x){return(x/sd(x))})
-    
-  }  
-  epd <- data.frame(epd)
-  colnames(epd) <- colnames(data)
-  return(epd)
-}
 
 threshold_data <- makeExceedances(test, thresholds = getThresholds(test, 0.8))
-getThresholds(test, 0.8)[4]
-length(which(test$humidity.Seattle > 100))
-
-threshold_data$humidity.Vancouver
 apply(threshold_data, MARGIN = 2, 
       FUN = function(x){length(which(x > 0.0)) / length(x)})
-
-#plot(threshold_data$pressure.Houston)
-#plot(energy_weather_merged$pressure.Boston)
 
 s.clusters <- rep(5, length(test[1,]))
 val.params <- findUnivariateParams(data = threshold_data, clusters_size = s.clusters)
@@ -183,113 +99,14 @@ val.params <- findUnivariateParams(data = threshold_data, clusters_size = s.clus
 val.params
 
 
-plgpd_unif_at_zero <- function(x, p.zero, alpha, beta, kappa){
-  if(p.zero < 0 | p.zero > 1) stop("p.zero should be between 0 and 1.")
-  if(x == 0)
-    return(runif(n = 1, min = 0, max = p.zero))
-  else{
-    return(p.zero + (1-p.zero)*(1-max(0, (1+sign(alpha)*x/(beta+kappa))^{-alpha})))
-  }
-}
-
-plgpd_unif_at_zero.row <- function(xs, p.zeroes, params.mat){
-  # params.mat contains alpha beta rho kappa
-  res <- rep(0, length(xs))
-  for(i in 1:length(xs)){
-    res[i] <- plgpd_unif_at_zero(x = xs[i],
-                    p.zero = p.zeroes[i],
-                    alpha = params.mat[i,1],
-                    beta = params.mat[i,2],
-                    kappa = params.mat[i,4])
-  }
-  
-  return(res)
-}
-
-computePZero <- function(params){
-  # Computes probability of having zero given univariate extreme value model
-  return(1-(1+params[,4]/params[,2])^{-abs(params[,1])})
-}
-
-makeConditionalMatrices <- function(data, exceedeances, q.s, 
-                                    horizon, save=T, params, n_samples = length(data[,1]),
-                                    name="conditional-matrices"){
-  
-  p.zeroes <- computePZero(params)
-  # epd_cdf <- apply(X = exceedeances, MARGIN = 1, 
-  #                  FUN = function(x){
-  #                    return(plgpd.row(xs = x, p.zeroes = p.zeroes, params.mat = params))
-  #                    }
-  #                  )
-  # epd_cdf <- t(epd_cdf)
-
-  exceedeances_cdf_ecdf <- exceedeances
-  for(i in 1:length(exceedeances[1,])){
-    # This creates ordered uniform samples in the same order 
-    # as in data.
-    exceedeances_cdf_ecdf[which(exceedeances[,i]==0), i] <- 
-      ecdf(data[which(exceedeances[,i]==0), i])(data[which(exceedeances[,i]==0), i]) * p.zeroes[i]
-  }
-
-  list_of_list_horizons <- list()
-  n_vars <- length(data[1,])
-  for(h in horizon){
-    list_of_matrices_conditional <- list()
-    # creates a square matrix with all the vars in
-    quantile.update.values <- matrix(0, 
-                                     nrow = length(exceedeances[1,]), 
-                                     ncol = length(exceedeances[1,]))
-    colnames(quantile.update.values) <- colnames(exceedeances)
-    rownames(quantile.update.values) <- colnames(exceedeances)
-    
-    for(i in 1:n_vars){
-      # creates a temporary matrix with cols equal to number of nvars + 1
-      # and rows such that the i-th component is an extreme
-      mat_temp <- matrix(0,
-                         nrow = length(which(exceedeances[1:(s.sample-h), i] > 0)),
-                         ncol = n_vars+1)
-      # filtering the i-th eCDF column with extremes 
-      temp <- exceedeances_cdf_ecdf[which(exceedeances[1:(s.sample-h), i] > 0), i]
-      # addting those values in the nvars + 1 column
-      mat_temp[,n_vars+1] <- ecdf(temp)(temp)
-      
-      for(j in 1:n_vars){
-        # filtering data of j-th vars when i-th is extreme h timesteps before
-        data_j <- exceedeances_cdf_ecdf[which(exceedeances[1:(s.sample-h), i] > 0)+h, j]
-        # computing the probability that j-th was an extreme as well 
-        # h timesteps after i-th was an extreme
-        quantile.update.values[i, j] <- mean(data_j <= q.s[j])
-        data_j <- ecdf(data_j)(data_j)
-        # saving the unif values of j-th var used here.
-        mat_temp[,j] <- data_j 
-      }
-      
-      colnames(mat_temp) <- c(colnames(exceedeances), colnames(exceedeances)[i])
-      list_of_matrices_conditional[[i]] <- mat_temp
-    }
-
-    
-    list_of_list_horizons[[h]] <- list(unif.values=list_of_matrices_conditional,
-                                       quantiles.values=quantile.update.values)
-  }
-  
-  if(save){
-    file_name <- paste(name, ".RData", sep="")
-    cat(paste("Backing up the conditional matrices as", file_name, "..."))
-    list.save(list_of_list_horizons,
-              file=file_name)
-    cat("done\n")
-  }
-  return(list_of_list_horizons)
-}
 
 horizon <- c(1,2,3,6,12,24)
 s.sample <- 40000
-cont_mat <- makeConditionalMatrices(data = test[,100:118],
-                                    exceedeances = threshold_data[,100:118],
-                                    q.s=getThresholds(test, 0.95)[100:118],
+cont_mat <- makeConditionalMatrices(data = test[,100:102],
+                                    exceedeances = threshold_data[,100:102],
+                                    q.s=getThresholds(test, 0.95)[100:102],
                                     horizon = horizon,
-                                    params = val.params[100:118,],
+                                    params = val.params[100:102,],
                                     n_samples = s.sample,
                                     name = "conditional-mat-test")
 
@@ -297,3 +114,171 @@ cont_mat <- makeConditionalMatrices(data = test[,100:118],
 cont_mat %>% print
 names(cont_mat)
 cont_mat[[1]]$quantiles.values
+
+
+############################################################################################
+################################## TRON PROBABILITIES ######################################
+############################################################################################
+
+
+### CREATION OF MATRICES
+
+horizon <- c(1,2,3,6,12,24)
+require(VineCopula)
+
+list_of_list_horizons <- list.load(file = "conditional-mat-test.RData")
+list_of_list_horizons_vines <- list()
+
+n_vars <- length(threshold_data[1,100:102])
+
+fitExceedancesVines <- function(exceedances, list_of_matrix){
+  #list_of_list_horizons <- list.load(file = "conditional-mat-test.RData")
+  list_of_list_horizons <- list_of_matrix
+  list_of_list_horizons_vines <- list()
+  
+  n_vars <- length(exceedances[1,])
+  
+  for(h in horizon){
+    list_of_vines_mat <- list()
+    cat("Horizon: ", h, "\n")
+    for(i in 1:n_vars){
+      cat("--->", colnames(exceedances)[i], " ")
+      list_of_vines_mat[[i]] <- RVineStructureSelect(
+        data = list_of_list_horizons[[h]]$unif.values[[i]], familyset = c(3,4), type = 0,
+        selectioncrit = "AIC", indeptest = TRUE, level = 0.05,
+        trunclevel = NA, progress = FALSE, weights = NA, treecrit = "tau",
+        se = FALSE, rotations = TRUE, method = "mle", cores = 7)
+      cat("DONE in", "TIME", " \n")
+    }
+    list_of_list_horizons_vines[[h]] <- list_of_vines_mat
+  }
+  
+  list.save(list_of_list_horizons_vines, file = "cond-mat-vines-12361224-v2.RData")
+  return(list_of_list_horizons_vines)
+}
+
+fitExceedancesVines(threshold_data[,100:102], list_of_list_horizons)
+
+list_of_list_horizons_vines_loaded <- list.load("cond-mat-vines-12361224-v2.RData")
+list_of_list_horizons <- list.load(file = "conditional-mat-test.RData")
+
+computeTRONwithLists <- function(exceedances, list_vines, list_of_matrix){
+  tron_probabilities <- list()
+  set.seed(42)
+  for(h in horizon){
+    tron_proba_matrix <- matrix(0, nrow = length(exceedances[1,]), ncol = length(exceedances[1,]))
+    colnames(tron_proba_matrix) <- colnames(exceedances)[1:(length(exceedances[1,]))]
+    rownames(tron_proba_matrix) <- colnames(exceedances)[1:(length(exceedances[1,]))]
+    tron_proba_matrix_sd <- tron_proba_matrix
+    cat(paste("Horizon", h, "\n"))
+    for(i in 1:n_vars){
+      cat(paste("--> extreme in", colnames(tron_proba_matrix)[i]), "...")
+      te.st <- RVineSim(RVM = list_vines[[h]][[i]], N = 100000)
+      te.st <- te.st[,1:(length(te.st[1,])-1)]
+      qq.values <- list_of_matrix[[h]]$quantiles.values[i,]
+      qq.values <- c(qq.values)
+      te.st <- t(apply(te.st, 1, function(x){x>qq.values}))
+      tron_proba_matrix[i,] <- apply(te.st, MARGIN = 2, mean)
+      tron_proba_matrix_sd[i,] <- (apply(te.st, MARGIN = 2, sd)/sqrt(length(te.st[,1])))
+      cat("\t done\n")
+    }
+    tron_probabilities[[h]] <- list(mean=tron_proba_matrix, sd=tron_proba_matrix_sd)
+  }
+  tron_probabilities[[1]]$mean
+  tron_probabilities[[1]]$sd
+  
+  list.save(tron_probabilities, file = "tron-cond-test.RData")
+  return(tron_probabilities)
+}
+
+tron_temp <- computeTRONwithLists(exceedances = threshold_data[,100:102],
+                                   list_vines = list_of_list_horizons_vines_loaded,
+                                   list_of_matrix = list_of_list_horizons)
+
+tron_temp[[12]]
+
+computeTRON <- function(data, exceedances, q.s, horizon, params, n_samples, name){
+  list_of_mat <- makeConditionalMatrices(data = data,
+                                      exceedeances = exceedances,
+                                      q.s=q.s,
+                                      horizon = horizon,
+                                      params = params,
+                                      n_samples = n_samples,
+                                      name = name)
+  # list_of_mat <- list.load("conditional-mat-test.Rdata")
+  list_vines <- fitExceedancesVines(exceedances = exceedances,
+                                    list_of_matrix = list_of_mat)
+  # list_vines <- list.load("cond-mat-vines-12361224-v2.RData")
+  tron <- computeTRONwithLists(exceedances = exceedances, 
+                               list_vines = list_vines,
+                               list_of_matrix = list_of_mat)
+  return(tron)
+}
+
+
+max_n_vars <- 110
+res <- computeTRON(data = test[,100:max_n_vars],
+                    exceedances = threshold_data[,100:max_n_vars],
+                      q.s=getThresholds(test, 0.95)[100:max_n_vars],
+                      horizon = horizon,
+                      params = val.params[100:max_n_vars,],
+                      n_samples = s.sample,
+                      name = "conditional-mat-test")
+
+
+
+
+
+
+
+# this is to observe the convergence speed
+tron_probabilities_N <- list()
+N_sims <- 2^(8:17)
+for(h in N_sims){
+  set.seed(42)
+  tron_proba_matrix <- matrix(0, nrow = length(epd[1,]), ncol = length(epd[1,]))
+  colnames(tron_proba_matrix) <- colnames(epd)
+  rownames(tron_proba_matrix) <- colnames(epd)
+  tron_proba_matrix_sd <- tron_proba_matrix
+  
+  for(i in 1:n_vars){
+    te.st <- RVineSim(RVM = list_of_list_horizons_vines_loaded[[6]][[i]], N = h)
+    qq.values <- list_of_list_horizons[[6]]$quantiles.values[i,]
+    qq.values <- c(qq.values, NA)
+    print(qq.values)
+    te.st <- t(apply(te.st, 1, function(x){x>qq.values}))
+    te.st <- te.st[,1:6]
+    #print(te.st)
+    tron_proba_matrix[i,] <- apply(te.st, 2, mean)[1:6]
+    tron_proba_matrix_sd[i,] <- (apply(te.st, 2, sd)/sqrt(length(te.st[,1])))[1:6]
+  }
+  tron_probabilities_N[[h]] <- list(mean=tron_proba_matrix, sd=tron_proba_matrix_sd)
+}
+
+par(mfrow=c(1,1), mar=c(4.1,4.1,0.5,0.5))
+plot(log(N_sims), log(vapply(N_sims, function(i){tron_probabilities_N[[i]]$sd[2,3]}, 1)),
+     xlab="Simulations", ylab="Standard deviation")
+abline( h = log(seq( -7, -2, 2^{-4})), lty = 3, col = colors()[ 440 ] )
+abline( v = seq( 0, 2^18, 2^10), lty = 3, col = colors()[ 440 ] )
+line(log(N_sims), log(vapply(N_sims, function(i){tron_probabilities_N[[i]]$sd[2,4]}, 1)))
+
+axis(2, at=x,labels=x, col.axis="red", las=2)
+
+
+
+### WORK with TRON probabilities
+
+setwd("C:/Users/Valentin/Documents/GitHub/multi-trawl-extremes/results/MMSEV/")
+for(i in 1:n_vars){
+  tron_p <- round(t(vapply(1:n_vars, function(h){tron_probabilities[[horizon[h]]]$mean[i,]},
+                           rep(0,n_vars))), digits = 3)
+  tron_sd <- round(t(vapply(1:n_vars, function(h){tron_probabilities[[horizon[h]]]$sd[i,]},
+                            rep(0,n_vars))), digits = 5)
+  colnames(tron_p) <- colnames(epd)
+  rownames(tron_p) <- horizon
+  colnames(tron_sd) <- colnames(epd)
+  rownames(tron_sd) <- horizon
+  
+  write.csv(tron_p, row.names = T, file = paste("tron_",colnames(epd)[i],".csv", sep = ""))
+  write.csv(tron_sd, row.names = T, file = paste("tron_",colnames(epd)[i],"_sd.csv", sep = ""))
+}
